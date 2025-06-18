6b5e4fb137daa35122656e0a919a4431b495225c
Jose Luis Rodriguez <joseluis.rodriguez@escuelaestech.es>
2025-06-18 17:24:46 +0200

    Clase snacks corregida

--- 01-maquina-espendedora/src/Snacks.java
+++ 01-maquina-espendedora/src/Snacks.java
@@ -2,31 +2,31 @@ import java.util.ArrayList;
 
 public class Snacks {
 
-    private ArrayList<Snack> snacks = new ArrayList<>();
+    private static final ArrayList<Snack> snacks;
+
+    //Bloque estatico inicializador
+    static {
+        snacks = new ArrayList<>();
+        snacks.add(new Snack("Patatas", 70));
+        snacks.add(new Snack("Refresco", 50));
+        snacks.add(new Snack("Sandwich", 120));
+    }
 
     public ArrayList<Snack> getSnacks() {
         return snacks;
     }
 
-    public boolean agregarSnack(Snack s){
-        boolean aniadido = false;
-        if (s != null){
-            snacks.add(s);
-            System.out.println("Snack añadido correctamente");
-            return true;
-        }else{
-            System.out.println("No se ha podido añadir el snack");
-            return false;
-        }
+    public static void agregarSnack(Snack s){
+        snacks.add(s);
     }
 
-    public void mostrarSnack(){
+    public static void mostrarSnack(){
         for (Snack snack : snacks){
             System.out.println(snack);
         }
     }
 
-    public Snack buscarSnackPorNombre(String nombre){
+    public static Snack buscarSnackPorNombre(String nombre){
         for (Snack snack : snacks){
             if (snack.getNombre().equalsIgnoreCase(nombre)){
                 return snack;
