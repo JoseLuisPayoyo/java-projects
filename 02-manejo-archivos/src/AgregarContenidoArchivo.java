343e682f9fd05e8a0529d5a9e834454be757cd95
Jose Luis Rodriguez <joseluis.rodriguez@escuelaestech.es>
2025-06-18 18:24:16 +0200

    Añado contenido al archivo

---
+++ 02-manejo-archivos/src/AgregarContenidoArchivo.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
+public class AgregarContenidoArchivo {
+    public static void main(String[] args) {
+        boolean anexar = false; //para que no se pierda la info que ya hay
+        var nombreArchivo = "mi_archivo.txt";
+        var archivo = new File(nombreArchivo);
+
+        try{
+            //revisar si existe el archivo
+            anexar = archivo.exists();
+            var salida = new PrintWriter(new FileWriter(archivo, anexar));
+            var nuevoContenido = "Nuevo\ncontenido";
+            salida.println(nuevoContenido);
+            salida.close();
+            System.out.println("Se agrego contenido al archivo");
+        } catch (Exception e) {
+            System.out.println("Error al imprimir el archivo: " + e.getMessage());
+        }
+    }
+}
