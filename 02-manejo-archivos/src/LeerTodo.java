521441a3aae63fce7b1695f43f41bc13c7c65fa2
Jose Luis Rodriguez <joseluis.rodriguez@escuelaestech.es>
2025-06-18 18:16:33 +0200

    Leer todo el archivo

---
+++ 02-manejo-archivos/src/LeerTodo.java
@@ -0,0 +1,20 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class LeerTodo {
+    public static void main(String[] args) {
+        var nomberArchivo = "mi_archivo.txt";
+
+        try{
+            //Leer todas las lineas del archivo
+            List<String> lineas = Files.readAllLines(Paths.get(nomberArchivo));
+            System.out.println("Contenido del archivo: ");
+            for (String linea : lineas){
+                System.out.println(linea);
+            }
+        }catch (Exception e){
+            System.out.println("Error al leer el archivo: " + e.getMessage());
+        }
+    }
+}
