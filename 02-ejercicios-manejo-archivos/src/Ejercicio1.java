cbd0939d9aa87c9e86f975dde9e62d1254f4bace
Jose Luis Rodriguez <joseluis.rodriguez@escuelaestech.es>
2025-06-25 11:52:00 +0200

    Ejercicio 1

--- 02-ejercicios-manejo-archivos/src/Ejercicio1.java
+++ 02-ejercicios-manejo-archivos/src/Ejercicio1.java
@@ -1,5 +1,26 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Ejercicio1 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String ruta = "C:\\projects\\java-projects\\02-ejercicios-manejo-archivos\\src\\registro_visitas.txt";
+        File registro = new File(ruta);
+
+        try {
+            LocalDateTime fecha = LocalDateTime.now();
+            String fechaFormateada = fecha.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+
+            //escribimos modo append
+            PrintWriter salida = new PrintWriter(new FileWriter(registro, true));
+            salida.println("[" + fechaFormateada + "] El programa fue ejecutado");
+            salida.close();
+
+            System.out.println("Ejecutado con exito");
+        } catch (Exception e) {
+            System.out.println("No se ha posido escribir el archivo: " + e.getMessage());
+        }
     }
 }
