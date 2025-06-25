7c0f71ad3e066be18eccbff667874d7d33a3e77e
Jose Luis Rodriguez <joseluis.rodriguez@escuelaestech.es>
2025-06-25 18:07:41 +0200

    Metodo eliminadr y modificar creado y probado

--- ZonaFit/src/main/java/zona_fit/datos/ClienteDAO.java
+++ ZonaFit/src/main/java/zona_fit/datos/ClienteDAO.java
@@ -100,11 +100,52 @@ public class ClienteDAO implements IClienteDAO{
 
     @Override
     public boolean modificarCliente(Cliente cliente) {
+        PreparedStatement ps;
+        Connection con = Conexion.getConnection();
+        var sql = "UPDATE cliente SET nombre=?, apellido=?, membresia=? " +
+                "WHERE id = ?";
+
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1, cliente.getNombre());
+            ps.setString(2, cliente.getApellido());
+            ps.setInt(3, cliente.getMembresia());
+            ps.setInt(4, cliente.getId());
+
+            ps.execute();
+            return true;
+        } catch (Exception e) {
+            System.out.println("Error al modificar cliente: " + e.getMessage());
+        } finally {
+            try {
+                con.close();
+            } catch (Exception e){
+                System.out.println("Error al cerrar conexion: " + e.getMessage());
+            }
+        }
         return false;
     }
 
     @Override
     public boolean eliminarCliente(Cliente cliente) {
+        PreparedStatement ps;
+        Connection con = Conexion.getConnection();
+        var sql = "DELETE FROM cliente WHERE id = ?";
+
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, cliente.getId());
+            ps.execute();
+            return true;
+        } catch (Exception e) {
+            System.out.println("Error al eliminar cliente: " + e.getMessage());
+        } finally {
+            try {
+                con.close();
+            } catch (Exception e){
+                System.out.println("Error al cerrar conexion: " + e.getMessage());
+            }
+        }
         return false;
     }
 
@@ -116,12 +157,39 @@ public class ClienteDAO implements IClienteDAO{
         clientes.forEach(System.out::println);
 
         //buscar por id
-        var cliente1 = new Cliente(2);
-        var encontrado = clienteDao.buscarClientePorId(cliente1);
-        if (encontrado) {
-            System.out.println("\nCliente encontrado:" + cliente1);
-        }else{
-            System.out.println("\nNo se encontro el cliente con el id: " + cliente1.getId());
-        }
+//        var cliente1 = new Cliente(2);
+//        var encontrado = clienteDao.buscarClientePorId(cliente1);
+//        if (encontrado) {
+//            System.out.println("\nCliente encontrado:" + cliente1);
+//        }else{
+//            System.out.println("\nNo se encontro el cliente con el id: " + cliente1.getId());
+//        }
+
+        //agregar cliente
+//        var nuevoCliente = new Cliente("Daniel", "Monete", 5);
+//        var agregado = clienteDao.agregarCliente(nuevoCliente);
+//        if (agregado){
+//            System.out.println("Cliente agregado\n");
+//        }else{
+//            System.out.println("No se agrego");
+//        }
+
+        //modificar cliente
+//        var modificarCliente = new Cliente(3,"Pepe", "Tejeda", 4);
+//        var modificado = clienteDao.modificarCliente(modificarCliente);
+//        if (modificado){
+//            System.out.println("Cliente modificado: " + modificarCliente);
+//        } else{
+//            System.out.println("No se modifico el cliente: " + modificarCliente);
+//        }
+
+        //eliminar cliente
+//        var clienteEliminar = new Cliente(3);
+//        var eliminado = clienteDao.eliminarCliente(clienteEliminar);
+//        if (eliminado){
+//            System.out.println("Cliente eliminado: " + clienteEliminar);
+//        }else{
+//            System.out.println("Cliente no eliminado: " + clienteEliminar);
+//        }
     }
 }
