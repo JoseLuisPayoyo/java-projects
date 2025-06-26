da7861f73f55d1821409ad385a6379ef705251dc
Jose Luis Rodriguez <joseluis.rodriguez@escuelaestech.es>
2025-06-26 12:11:54 +0200

    Zona fit usando spring. Configuarada la clase servicio

---
+++ ZonaFitSpring/src/main/java/jose/zona_fit/servicio/ClienteServicio.java
@@ -0,0 +1,38 @@
+package jose.zona_fit.servicio;
+
+import jose.zona_fit.modelo.Cliente;
+import jose.zona_fit.repositorio.ClienteRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ClienteServicio implements IClienteServicio{
+
+    @Autowired //auto inyectar
+    private ClienteRepositorio clienteRepositorio;
+
+    @Override
+    public List<Cliente> listarCliente() {
+        List<Cliente> clientes = clienteRepositorio.findAll();
+        return clientes;
+    }
+
+    @Override
+    public Cliente buscarClientePorId(Integer idCliente) {
+        Cliente cliente = clienteRepositorio.findById(idCliente).orElse(null);
+        return null;
+    }
+
+    @Override
+    public void guardarCliente(Cliente cliente) {
+        clienteRepositorio.save(cliente);
+    }
+
+    @Override
+    public void eliminarCliente(Cliente cliente) {
+        clienteRepositorio.delete(cliente);
+    }
+}
--- ZonaFitSpring/src/main/java/jose/zona_fit/servicio/IClienteServicio.java
+++ ZonaFitSpring/src/main/java/jose/zona_fit/servicio/IClienteServicio.java
@@ -1,4 +1,16 @@
 package jose.zona_fit.servicio;
 
+import jose.zona_fit.modelo.Cliente;
+
+import java.util.List;
+
 public interface IClienteServicio {
+
+    public List<Cliente> listarCliente();
+
+    public Cliente buscarClientePorId(Integer idCliente);
+
+    public void guardarCliente(Cliente cliente);
+
+    public void eliminarCliente(Cliente cliente);
 }
