ee7e2ac8298f134f85d435cb3b2626afd1ce7e70
Jose Luis Rodriguez <joseluis.rodriguez@escuelaestech.es>
2025-06-27 12:34:11 +0200

    Cambiando a swim la app de zona fit

---
+++ ZonaFitSpring/src/main/java/jose/zona_fit/ZonaFitSwim.java
@@ -0,0 +1,27 @@
+package jose.zona_fit;
+
+import jose.zona_fit.gui.ZonaFitForma;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class ZonaFitSwim {
+    public static void main(String[] args) {
+        //Instanciar la fabrica de spring
+        ConfigurableApplicationContext contextoSpring = new SpringApplicationBuilder(ZonaFitSwim.class)
+                .headless(false)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        //Crear objeto de swim
+        SwingUtilities.invokeLater(()-> {
+            ZonaFitForma zonaFitForma = contextoSpring.getBean(ZonaFitForma.class);
+            zonaFitForma.setVisible(true);
+        });
+    }
+}
