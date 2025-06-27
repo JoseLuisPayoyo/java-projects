408e993b0a5f87671c11fda5b392be9126d1f916
Jose Luis Rodriguez <joseluis.rodriguez@escuelaestech.es>
2025-06-27 13:26:13 +0200

    Implementado el metodo de guardar clientes con el boton

--- ZonaFitSpring/src/main/java/jose/zona_fit/gui/ZonaFitForma.java
+++ ZonaFitSpring/src/main/java/jose/zona_fit/gui/ZonaFitForma.java
@@ -1,5 +1,6 @@
 package jose.zona_fit.gui;
 
+import jose.zona_fit.modelo.Cliente;
 import jose.zona_fit.servicio.ClienteServicio;
 import jose.zona_fit.servicio.IClienteServicio;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,11 +8,19 @@ import org.springframework.stereotype.Component;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 @Component
 public class ZonaFitForma extends  JFrame{
     private JPanel panelPrincipal;
     private JTable clientesTabla;
+    private JTextField nombreTexto;
+    private JTextField apellidoTexto;
+    private JTextField membresiaTexto;
+    private JButton guardarButton;
+    private JButton eliminarButton;
+    private JButton limpiarButton;
     IClienteServicio clienteServicio;
     private DefaultTableModel tablaModeloClientes;
 
@@ -19,6 +28,7 @@ public class ZonaFitForma extends  JFrame{
     public ZonaFitForma(ClienteServicio clienteServicio){
         this.clienteServicio = clienteServicio;
         iniciarForma();
+        guardarButton.addActionListener(e -> guardarCliente());
     }
 
     private void iniciarForma(){
@@ -27,6 +37,7 @@ public class ZonaFitForma extends  JFrame{
         setSize(900, 700);
         setLocationRelativeTo(null); //centrar ventana
     }
+
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
@@ -50,5 +61,39 @@ public class ZonaFitForma extends  JFrame{
             };
             this.tablaModeloClientes.addRow(renglonCliente);
         });
+    }
+
+    private void guardarCliente(){
+        if (nombreTexto.getText().equals("")){
+            mostrarMensaje("Proporciona un nombre");
+            nombreTexto.requestFocusInWindow();
+            return;
+        }
+        if (membresiaTexto.getText().equals("")){
+            mostrarMensaje("Proporciona una membresia");
+            membresiaTexto.requestFocusInWindow();
+            return;
+        }
+        //recuperar valores
+        var nombre = nombreTexto.getText();
+        var apellido = apellidoTexto.getText();
+        var membresia = Integer.parseInt(membresiaTexto.getText());
+        var cliente = new Cliente();
+        cliente.setNombre(nombre);
+        cliente.setApellido(apellido);
+        cliente.setMembresia(membresia);
+        this.clienteServicio.guardarCliente(cliente);
+        limpiarFormulario();
+        listarClientes();
+    }
+
+    private void mostrarMensaje(String mensaje){
+        JOptionPane.showMessageDialog(this, mensaje);
+    }
+
+    private void limpiarFormulario(){
+        nombreTexto.setText("");
+        apellidoTexto.setText("");
+        membresiaTexto.setText("");
     }
 }
